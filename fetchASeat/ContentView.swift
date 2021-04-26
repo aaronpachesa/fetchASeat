932e4e1f6e04e94ca5301d401295f00254948358
Aaron Pachesa <aaronpachesa@me.com>
2021-04-26 15:07:19 -0400

    added an overlay to the search bar (magnifying icon and cross icon)

--- fetchASeat/ContentView.swift
+++ fetchASeat/ContentView.swift
@@ -19,23 +19,41 @@ struct ContentView: View {
                 .background(Color(.systemGray6))
                 .foregroundColor(.gray)
                 .cornerRadius(8)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+                        
+                        if isEditing {
+                            Button(action: {
+                                self.text = ""
+                            }) {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            }
+                        }
+                    }
+                )
                 .padding(.horizontal, 10)
                 .onTapGesture {
                     self.isEditing = true
                 }
             
             if isEditing {
-                            Button(action: {
-                                self.isEditing = false
-                                self.text = ""
-
-                            }) {
-                                Text("Cancel")
-                            }
-                            .padding(.trailing, 10)
-                            .transition(.move(edge: .trailing))
-                            .animation(.default)
-                        }
+                Button(action: {
+                    self.isEditing = false
+                    self.text = ""
+                    
+                }) {
+                    Text("Cancel")
+                }
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+                .animation(.default)
+            }
         }
     }
 }
