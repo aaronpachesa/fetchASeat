71456a48227ee33de55e9509b0e5ff1e0fc8eec9
Aaron Pachesa <aaronpachesa@me.com>
2021-04-26 13:16:22 -0400

    Initial Commit

---
+++ fetchASeat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fetchASeat
+//
+//  Created by Aaron Pachesa on 4/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fetchASeat/fetchASeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchASeatApp.swift
+//  fetchASeat
+//
+//  Created by Aaron Pachesa on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct fetchASeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
