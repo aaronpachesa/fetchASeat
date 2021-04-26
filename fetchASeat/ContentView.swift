f565912c9e88f917fe86cfe8bd2d2e177520965a
Aaron Pachesa <aaronpachesa@me.com>
2021-04-26 14:34:56 -0400

    added the cancel button

--- fetchASeat/ContentView.swift
+++ fetchASeat/ContentView.swift
@@ -12,16 +12,30 @@ struct ContentView: View {
     @State private var text = ""
     var body: some View {
         HStack {
-            TextField("Search ...", text: $text)
+            TextField("Search events", text: $text)
+                .frame(width: 200)
                 .padding(7)
-//                .padding(.horizontal, 25)
+                .padding(.horizontal, 25)
                 .background(Color(.systemGray6))
                 .foregroundColor(.gray)
                 .cornerRadius(8)
-//                .padding(.horizontal, 10)
+                .padding(.horizontal, 10)
                 .onTapGesture {
                     self.isEditing = true
                 }
+            
+            if isEditing {
+                            Button(action: {
+                                self.isEditing = false
+                                self.text = ""
+
+                            }) {
+                                Text("Cancel")
+                            }
+                            .padding(.trailing, 10)
+                            .transition(.move(edge: .trailing))
+                            .animation(.default)
+                        }
         }
     }
 }
