bd5f39372bec72618abe8e68f57c9edee67b02cd
Aaron Pachesa <aaronpachesa@me.com>
2021-04-26 13:47:52 -0400

    added a search bar

--- fetchASeat/ContentView.swift
+++ fetchASeat/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isEditing = false
+    @State private var text = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            TextField("Search ...", text: $text)
+                .padding(7)
+//                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .foregroundColor(.gray)
+                .cornerRadius(8)
+//                .padding(.horizontal, 10)
+                .onTapGesture {
+                    self.isEditing = true
+                }
+        }
     }
 }
 
