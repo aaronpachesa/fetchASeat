4301889e2f527df437236ce33ea1746e29fb00ce
Aaron Pachesa <aaronpachesa@me.com>
2021-04-29 15:58:24 -0400

    added a needed loadData

--- fetchASeat/ContentView.swift
+++ fetchASeat/ContentView.swift
@@ -147,6 +147,9 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onAppear() {
+                    loadData()
+                }
                 .navigationTitle("")
                 .navigationBarHidden(true)
             }
